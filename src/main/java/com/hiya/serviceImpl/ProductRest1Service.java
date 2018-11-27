de404c8aa16acd4d9984558b1d5c26969f5cb91c
caozhijun <caozhijun@caozhijun-book>
2018-11-27 20:23:10 +0800

    https://github.com/13802706376/HiyaDubboProvider.git

---
+++ src/main/java/com/hiya/serviceImpl/ProductRest1Service.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright 1999-2014 dangdang.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hiya.serviceImpl;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import com.alibaba.dubbo.rpc.protocol.rest.support.ContentType;
+import com.hiya.dubbo.product.IProductRest1;
+import com.hiya.dubbo.product.Product;
+
+@Path("product")
+@Consumes({MediaType.APPLICATION_JSON, MediaType.TEXT_XML})
+@Produces({ContentType.APPLICATION_JSON_UTF_8, ContentType.TEXT_XML_UTF_8})
+public class ProductRest1Service implements IProductRest1 
+{
+
+    @GET
+    @Path("{id : \\d+}")
+    public Product getProduct(@PathParam("id") Long id) 
+    {
+        return new Product(id, "James");
+    }
+
+    @POST
+    @Path("register")
+    public int saveProduct(Product product)
+    {
+        System.out.println(product.toString());
+        return 200;
+    }
+}
---
+++ src/test/java/com/hiya/dubbo/test/ProviderStart.java
@@ -0,0 +1,9 @@
+package com.hiya.dubbo.test;
+
+public class ProviderStart 
+{
+	public static void main(String[] args) 
+	{
+        com.alibaba.dubbo.container.Main.main(args);
+    }
+}
